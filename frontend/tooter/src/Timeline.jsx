335b7a1d250787dddcdf382d8b89e95b41a7b947
phi9009 <phi9009@gmail.com>
2024-03-20 20:38:34 -0500

    continued working of frontend. added 'toots' to UI

--- frontend/tooter/src/Timeline.jsx
+++ frontend/tooter/src/Timeline.jsx
@@ -1,7 +1,46 @@
+import {v4 as uuidv4} from 'uuid';
+import Toot from './Toot';
+ 
+
 export default function Timeline (){
+    const test_user = {
+        _id: uuidv4(),
+        userName: "Phil",
+        userHandle: "pho9008"
+    }
+    const toots = [
+        {
+            _id: uuidv4(),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            user: {...test_user},
+            
+        },
+        {
+            _id: uuidv4(),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            user: {...test_user},
+            
+        },
+        {
+            _id: uuidv4(),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            user: {...test_user},
+            
+        },
+        {
+            _id: uuidv4(),
+            content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            user: {...test_user},
+        }
+    ]
+    console.log(toots)
     return (
         <div className="col-4 border">
-            Timeline placeholder
+            {toots.map((toot) => {
+                return(
+                <Toot key={toot._id} userName={toot.user.userName} userHandle={toot.user.userHandle} content={toot.content} />
+                )
+            })}
         </div>
     )
 }
---
+++ frontend/tooter/src/Toot.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Toot({content, userName, userHandle}){
+    console.log(userHandle)
+    return (
+        <div className="container border-top border-bottom p-2" >
+            <span className="h6">{userName}</span> 
+            <span className="text-secondary">@{userHandle}</span>
+            <p>{content}</p>
+
+        </div>
+)
+
+}
